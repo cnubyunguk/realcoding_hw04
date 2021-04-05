f8f7977da7af26d7faf9394c319fc69a89f8f3b2
cnubyunguk <byunguk.choi.cnu@gmail.com>
2021-04-06 02:22:27 +0900

    project setup

---
+++ src/main/java/org/cnu/realcoding/homework0420/Homework0420Application.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding.homework0420;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework0420Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework0420Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/cnu/realcoding/homework0420/Homework0420ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding.homework0420;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework0420ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
