ac12edd294699d8f66a373e1f9be93d148bc0276
cnubyunguk <byunuk.choi.cnu@gmail.com>
2021-04-06 15:35:26 +0900

    add dog controller & service
    - 개 정보를 신규등록할 수 있는  API 추가
    - 개 정보를 통째로 덮어쓸수있는 API 추가
    - 개 정보를 인자 3개로 받아올수있는 API 추가
    - 개 정보를 이름으로 받아올수있는 API 추가

---
+++ src/main/java/org/cnu/realcoding/homework0420/controller/DogController.java
@@ -0,0 +1,44 @@
+package org.cnu.realcoding.homework0420.controller;
+
+import org.cnu.realcoding.homework0420.domain.Dog;
+import org.cnu.realcoding.homework0420.service.DogManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class DogController {
+
+    @Autowired
+    private DogManagementService dogManagementService;
+
+    @PostMapping("/dogs")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createDog(@RequestBody Dog dog) {
+        dogManagementService.insertDog(dog);
+    }
+
+    @GetMapping("/dogs")
+    @ResponseStatus(HttpStatus.OK)
+    public Dog getDog(@RequestParam(value="name") String name, @RequestParam(value="ownerName") String ownerName,
+                             @RequestParam(value="ownerPhoneNumber") String ownerPhoneNumber){
+        return dogManagementService.getDog(name, ownerName, ownerPhoneNumber);
+    }
+
+    @GetMapping("/dogs/{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public Dog getDogByName(@RequestParam(value="name") String name)
+    {
+        return dogManagementService.getDogByName(name);
+    }
+
+    @PutMapping("/dogs/{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateDog(@PathVariable String name, @RequestBody Dog body){
+        dogManagementService.updateDog(name, body);
+    }
+}
---
+++ src/main/java/org/cnu/realcoding/homework0420/service/DogManagementService.java
@@ -0,0 +1,51 @@
+package org.cnu.realcoding.homework0420.service;
+
+import org.cnu.realcoding.homework0420.domain.Dog;
+import org.cnu.realcoding.homework0420.exception.DogNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DogManagementService {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Dog insertDog(Dog body){
+        if (getDog(body.getName(), body.getOwnerName(), body.getOwnerPhoneNumber()) != null)
+            throw new DogNotFoundException();
+
+        return mongoTemplate.insert(body);
+    }
+
+    public Dog getDog(String name, String ownerName, String ownerPhoneNumber){
+        Query query = new Query().addCriteria(Criteria.where("name").is(name));
+        query.addCriteria(Criteria.where("ownerName").is(ownerName));
+        query.addCriteria(Criteria.where("ownerPhoneNumber").is(ownerPhoneNumber));
+        return mongoTemplate.findOne(query, Dog.class);
+    }
+
+    public Dog getDogByName(String name){
+        Query query = new Query().addCriteria(Criteria.where("name").is(name));
+        return mongoTemplate.findOne(query, Dog.class);
+    }
+
+    public void updateDog(String name, Dog body) {
+        // TODO: 데이터베이스에 두번 접근하지 않는 다른 방법이 있을지?..
+        if (getDogByName(name) == null)
+            throw new DogNotFoundException();
+
+        Query query = new Query().addCriteria(Criteria.where("name").is(name));
+        Update update = new Update().
+                set("name", body.getName()).
+                set("kind", body.getKind()).
+                set("ownerName", body.getOwnerName()).
+                set("ownerPhoneNumber", body.getOwnerPhoneNumber());
+
+        mongoTemplate.updateFirst(query, update, Dog.class);
+    }
+}
